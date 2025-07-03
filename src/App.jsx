bb5add37cabb03fd31b6a294f719cfa6ef94916a
Elias-Rodriguez511 <elias_1305@gmail.com>
2025-07-03 13:07:49 -0400

    api-pokemon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PokemonFetcher from "./PokemonFetcher";
+
+function App(){
+  return(
+    <>
+      <h1>Conoce a tus Pokémon!</h1>
+      <PokemonFetcher/>
+    </>
+  );
+}
+
+export default App
